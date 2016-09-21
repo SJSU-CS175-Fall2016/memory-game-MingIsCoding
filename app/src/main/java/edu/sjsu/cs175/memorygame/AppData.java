64672e966f17cc7c587dbcdd387ff36373796d5a
MingTang <tmartin@126.com>
2016-09-20 23:15:55 -0700

    final

--- app/src/main/java/edu/sjsu/cs175/memorygame/AppData.java
+++ app/src/main/java/edu/sjsu/cs175/memorygame/AppData.java
@@ -1,9 +1,11 @@
 package edu.sjsu.cs175.memorygame;
 
+import java.io.Serializable;
+
 /**
  * Created by Ming on 9/13/16.
  */
-public class AppData {
+public class AppData implements Serializable{
     static boolean isStarted = false;
     static IconButton[][] mIconButtonsArray = null;
     static int score = 0;
